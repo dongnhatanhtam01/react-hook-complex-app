9a724444c0358ee60203354980b8adcabaf2ef2b
tamdna <tamdna@vietmap.vn>
2021-02-02 00:38:27 +0700

    reusabled component Container

---
+++ app/components/Container.js
@@ -0,0 +1,12 @@
+import React from "react"
+/**
+ * Container để chứa các reusabled component
+ */
+function Container (props) {
+  return (
+    <div className={"container py-md-5"+ (props.wide ? '': ' container--narrow')}>
+      {props.children}
+    </div>
+  )
+}
+export default Container
