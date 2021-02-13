cc52644ef92e3bcfe50b7d247c8060bc1dedab4e
tamdna <tamdna@vietmap.vn>
2021-02-13 12:23:05 +0700

    ở HomeGuest kết nối với back-end thông qua Axios.post

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -1,7 +1,25 @@
-import React from "react"
+import React, {useState} from "react"
 import UseEffectPage from "./useEffectPage"
+import Axios from "axios"
 
 function HomeGuest() {
+  
+  async function handleSubmit(e) {
+    e.preventDefault()
+    alert("hello..!")
+    
+    try {
+      await Axios.post("http://localhost:8080/register", { username:'test2', email:"test2@test.com", password:"qwerty123456" }).then((response) => {
+        console.log(response.data);
+      }, (error) => {
+        console.log(error);
+      })
+
+      console.log("User was successfully created.")
+    } catch (e) {
+      console.log(e.response.data)
+    }
+  }
   return (
     <UseEffectPage title="HomeGuest" wide={true} >
       <div className="row align-items-center">
@@ -10,7 +28,7 @@ function HomeGuest() {
           <p className="lead text-muted">Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again.</p>
         </div>
         <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="username-register" className="text-muted mb-1">
                 <small>Username</small>
