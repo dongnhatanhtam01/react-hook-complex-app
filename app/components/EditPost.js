a06853b90e74260d6e8c2098c14dda97672f3ad6
unknown <dongnhatanhtam01@gmail.com>
2021-04-18 18:08:17 +0700

    validate onBlur có lỗ hổng, khắc phục bằng cách thêm rule bắt nếu không có title

--- app/components/EditPost.js
+++ app/components/EditPost.js
@@ -8,126 +8,137 @@ import DispatchContext from "../DispatchContext"
 import UseEffectPage from "./UseEffectPage"
 
 function EditPost(props) {
-  const appState = useContext(StateContext)
-  const appDispatch = useContext(DispatchContext)
+ const appState = useContext(StateContext)
+ const appDispatch = useContext(DispatchContext)
 
-  const originalState = {
-    title: {
-      value: "",
-      hasErrors: false,
-      message: ""
-    },
-    body: {
-      value: "",
-      hasErrors: false,
-      message: ""
-    },
-    isFetching: true,
-    isSaving: false,
-    id: useParams().id,
-    sendCount: 0
+ const originalState = {
+  title: {
+   value: "",
+   hasErrors: false,
+   message: ""
+  },
+  body: {
+   value: "",
+   hasErrors: false,
+   message: ""
+  },
+  isFetching: true,
+  isSaving: false,
+  id: useParams().id,
+  sendCount: 0
+ }
+
+ function ourReducer(draft, action) {
+  switch (action.type) {
+   case "fetchComplete":
+    draft.title.value = action.value.title
+    draft.body.value = action.value.body
+    draft.isFetching = false
+    return
+   case "titleChange":
+    draft.title.value = action.value
+    return
+   case "bodyChange":
+    draft.body.value = action.value
+    return
+   case "submitRequest":
+    if(!draft.title.hasErrors && !draft.body.hasErrors) {
+     draft.sendCount++
+    }
+    return
+   case "saveRequestStarted":
+    draft.isSaving = true
+    return
+   case "saveRequestFinished":
+    draft.isSaving = false
+    return
+   case "titleRules":
+    if (!action.value.trim()) {
+     draft.title.hasErrors = true
+     draft.title.message = "You must provide title."
+    }
+    return
   }
+ }
 
-  function ourReducer(draft, action) {
-    switch (action.type) {
-      case "fetchComplete":
-        draft.title.value = action.value.title
-        draft.body.value = action.value.body
-        draft.isFetching = false
-        return
-      case "titleChange":
-        draft.title.value = action.value
-        return
-      case "bodyChange":
-        draft.body.value = action.value
-        return
-      case "submitRequest":
-        draft.sendCount++
-        return
-      case "saveRequestStarted":
-        draft.isSaving = true
-        return
-      case "saveRequestFinished":
-        draft.isSaving = false
-        return
-    }
+ const [state, dispatch] = useImmerReducer(ourReducer, originalState)
+
+ function submitHandler(e) {
+  e.preventDefault()
+  // validate onBlur có lỗ hổng, khắc phục bằng cách thêm rule bắt nếu không có title
+  dispatch({ type: "titleRules", value: state.title.value })
+  dispatch({ type: "submitRequest" })
+ }
+
+ useEffect(() => {
+  const ourRequest = Axios.CancelToken.source()
+  async function fetchPost() {
+   try {
+    const response = await Axios.get(`/post/${state.id}`, { cancelToken: ourRequest.token })
+    dispatch({ type: "fetchComplete", value: response.data })
+   } catch (e) {
+    console.log("There was a problem or the request was cancelled.")
+   }
   }
-
-  const [state, dispatch] = useImmerReducer(ourReducer, originalState)
-
-  function submitHandler(e) {
-    e.preventDefault()
-    dispatch({ type: "submitRequest" })
+  fetchPost()
+  return () => {
+   ourRequest.cancel()
   }
+ }, [])
 
-  useEffect(() => {
-    const ourRequest = Axios.CancelToken.source()
-    async function fetchPost() {
-      try {
-        const response = await Axios.get(`/post/${state.id}`, { cancelToken: ourRequest.token })
-        dispatch({ type: "fetchComplete", value: response.data })
-      } catch (e) {
-        console.log("There was a problem or the request was cancelled.")
-      }
+ useEffect(() => {
+  if (state.sendCount) {
+   dispatch({ type: "saveRequestStarted" })
+   const ourRequest = Axios.CancelToken.source()
+   async function fetchPost() {
+    try {
+     const response = await Axios.post(`/post/${state.id}/edit`, { title: state.title.value, body: state.body.value, token: appState.user.token }, { cancelToken: ourRequest.token })
+     // alert("congrat, post data complete...")
+     dispatch({ type: "saveRequestFinished" })
+     appDispatch({ type: "FLASH_MESSAGE_ACTION", value: "Post was updated." })
+     props.history.push(`/post/${state.id}`)
+    } catch (e) {
+     console.log("There was a problem or the request was cancelled.")
     }
-    fetchPost()
-    return () => {
-      ourRequest.cancel()
-    }
-  }, [])
+   }
+   fetchPost()
+   return () => {
+    ourRequest.cancel()
+   }
+  }
+ }, [state.sendCount])
 
-  useEffect(() => {
-    if (state.sendCount) {
-      dispatch({ type: "saveRequestStarted" })
-      const ourRequest = Axios.CancelToken.source()
-      async function fetchPost() {
-        try {
-          const response = await Axios.post(`/post/${state.id}/edit`, { title: state.title.value, body: state.body.value, token: appState.user.token }, { cancelToken: ourRequest.token })
-          // alert("congrat, post data complete...")
-          dispatch({ type: "saveRequestFinished" })
-          appDispatch({ type: "FLASH_MESSAGE_ACTION", value: "Post was updated." })
-          props.history.push(`/post/${state.id}`)
-        } catch (e) {
-          console.log("There was a problem or the request was cancelled.")
-        }
-      }
-      fetchPost()
-      return () => {
-        ourRequest.cancel()
-      }
-    }
-  }, [state.sendCount])
-
-  if (state.isFetching)
-    return (
-      <UseEffectPage title="...">
-        <LoadingDotsIcon />
-      </UseEffectPage>
-    )
-
+ if (state.isFetching)
   return (
-    <UseEffectPage title="Edit Post">
-      <form onSubmit={submitHandler}>
-        <div className="form-group">
-          <label htmlFor="post-title" className="text-muted mb-1">
-            <small>Title</small>
-          </label>
-          <input onChange={e => dispatch({ type: "titleChange", value: e.target.value })} value={state.title.value} autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="post-body" className="text-muted mb-1 d-block">
-            <small>Body Content</small>
-          </label>
-          <textarea onChange={e => dispatch({ type: "bodyChange", value: e.target.value })} name="body" id="post-body" className="body-content tall-textarea form-control" type="text" value={state.body.value} />
-        </div>
-
-        <button className="btn btn-primary" disabled={state.isSaving}>
-          Save Updates
-        </button>
-      </form>
-    </UseEffectPage>
+   <UseEffectPage title="...">
+    <LoadingDotsIcon />
+   </UseEffectPage>
   )
+
+ return (
+  <UseEffectPage title="Edit Post">
+   <form onSubmit={submitHandler}>
+    <div className="form-group">
+     <label htmlFor="post-title" className="text-muted mb-1">
+      <small>Title</small>
+     </label>
+     <input onBlur={e => dispatch({ type: "titleRules", value: e.target.value })} onChange={e => dispatch({ type: "titleChange", value: e.target.value })} value={state.title.value} autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
+     {state.title.hasErrors && <div className="alert alert-danger small invalidFeedback">{state.title.message}</div>}
+    </div>
+
+    <div className="form-group">
+     <label htmlFor="post-body" className="text-muted mb-1 d-block">
+      <small>Body Content</small>
+     </label>
+     <textarea onChange={e => dispatch({ type: "bodyChange", value: e.target.value })} name="body" id="post-body" className="body-content tall-textarea form-control" type="text" value={state.body.value} />
+    </div>
+
+    <button className="btn btn-primary" disabled={state.isSaving}>
+     Save Updates
+        </button>
+   </form>
+  </UseEffectPage>
+ )
 }
 
 export default EditPost
