e00448f254456e7fba4c5fcd809f86e7a21629f2
unknown <dongnhatanhtam01@gmail.com>
2021-04-29 01:36:46 +0700

    trường hợp useEffect ondemand state.searchTerm setState kèm đk setTimeout, sau 3 giây cứ log giá trị thay đổi 1 lần (chạy hàm side effect) chưa có clear mỗi lần thay đổi (chưa unmount) kết quả log ra khác

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -23,18 +23,18 @@ function HeaderLoggedIn(props) {
  }
  return (
   <div className="flex-row my-3 my-md-0">
-   <a data-for="search" data-tip="Search"  onClick={handleSearchIcon} href="#" className="text-white mr-2 header-search-icon">
+   <a data-for="search" data-tip="Search" onClick={handleSearchIcon} href="#" className="text-white mr-2 header-search-icon">
     <i className="fas fa-search"></i>
    </a>
-   <ReactTooltip   place="bottom" id="search" className="custom-tooltip" />
-   <span className="mr-2 header-chat-icon text-white">
+   <ReactTooltip place="bottom" id="search" className="custom-tooltip" />
+   {" "}<span className="mr-2 header-chat-icon text-white">
     <i className="fas fa-comment"></i>
     <span className="chat-count-badge text-white"> </span>
    </span>
-   <NavLink to={`/profile/${appState.user.username}`} className="mr-2">
+   {" "}<NavLink to={`/profile/${appState.user.username}`} className="mr-2">
     <img className="small-header-avatar" src={appState.user.avatar} />
    </NavLink>
-   <NavLink className="btn btn-sm btn-success mr-2" to="/create-new-post">
+   {" "}<NavLink className="btn btn-sm btn-outline-warning mr-2" to="/create-new-post">
     Create Post
           </NavLink>
    <button onClick={() => handleLogout()} className="btn btn-sm btn-secondary">
--- app/components/Search.js
+++ app/components/Search.js
@@ -1,31 +1,68 @@
 import React, { useContext, useEffect } from "react"
 import DispatchContext from "../DispatchContext"
-
+import { useImmer } from "use-immer";
 function Search() {
  const appDispatch = useContext(DispatchContext)
+
+ const [state, setState] = useImmer({
+  searchTerm: '',
+  result: [],
+  show: 'neither',
+  requestCount: 0
+ })
+
  function handleTimeCircleIcon(e) {
   e.preventDefault()
-  appDispatch({type:"CLOSE_SEARCH"})
+  appDispatch({ type: "CLOSE_SEARCH" })
  }
+ // execute after each render
  useEffect(() => {
+  // do side-effect here
   document.addEventListener("keyup", searchKeyPressHandler)
-  // clean up browser after proper time
+  // clean up here...
+  // execute before the next render or unmount
   return () => document.removeEventListener("keyup", searchKeyPressHandler)
  }, [])
- function searchKeyPressHandler(e) {
-  if(e.keyCode == 27) {
-   appDispatch({type:"CLOSE_SEARCH"})
+
+
+
+ useEffect(() => {
+  const delay = setTimeout(() => {
+   console.log(state.searchTerm);
+   // setState(draft => {
+   //  draft.requestCount++
+   // })
+  }, 3000)
+  // return () => clearTimeout(delay)
+ }, [state.searchTerm])
+
+ useEffect(() => {
+  if (state.requestCount) {
+   // Send axios request here
+   console.log(state.requestCount);
   }
+ }, [state.requestCount])
+
+ function searchKeyPressHandler(e) {
+  if (e.keyCode == 27) {
+   appDispatch({ type: "CLOSE_SEARCH" })
+  }
+ }
+ function handleInputField(e) {
+  const value = e.target.value
+  setState(draft => {
+   draft.searchTerm = value
+  })
  }
 
  return (
   <div className="search-overlay">
-   <div className="search-overlay-top shadow-sm">
+   <div className="search-overlay-top shadow-sm bg-dark">
     <div className="container container--narrow">
      <label htmlFor="live-search-field" className="search-overlay-icon">
       <i className="fas fa-search" />
      </label>
-     <input autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
+     <input onChange={handleInputField} autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
      <span onClick={handleTimeCircleIcon} className="close-live-search">
       <i className="fas fa-times-circle" />
      </span>
@@ -35,7 +72,7 @@ function Search() {
     <div className="container container--narrow py-3">
      <div className="live-search-results live-search-results--visible">
       <div className="list-group shadow-sm">
-       <div className="list-group-item active"><strong>Search Results</strong> (3 items found)</div>
+       <div className="list-group-item active bg-success "><strong>Search Results</strong> (3 items found)</div>
        <a href="#" className="list-group-item list-group-item-action">
         <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #1</strong>
         <span className="text-muted small">by brad on 2/10/2020 </span>
