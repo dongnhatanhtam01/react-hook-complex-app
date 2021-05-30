28c503a52e4b05b863949f4f276cc85a2f0a17f0
dongnhatanhtam01 <dongnhatanhtam01@gmail.com>
2021-05-31 00:29:18 +0700

    code họp chát thêm state fieldValue, chatMessage state.username == appState.user.username thì hiện chat-self không thì hiện ch

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -1,16 +1,46 @@
 import React, { useContext, useEffect, useRef, useState } from "react"
 import StateContext from "../StateContext"
 import DispatchContext from "../DispatchContext"
+import { useImmer } from "use-immer"
 
 function Chat() {
   const chatField = useRef(null)
   const appState = useContext(StateContext)
   const appDispatch = useContext(DispatchContext)
+
+  const [state, setState] = useImmer({
+    fieldValue: '',
+    chatMessages: []
+  })
+
   useEffect(() => {
-    if(appState.isChatOpen) {
+    if (appState.isChatOpen) {
       chatField.current.focus()
     }
   }, [appState.isChatOpen])
+
+
+  function handleFieldChange(e) {
+    const value = e.target.value
+    setState((draft) => {
+      // draft.chatMessages.push({ message: draft.fieldValue, username: appState.user.username, avatar: appState.user.avatar })
+      draft.fieldValue = value
+    })
+  }
+
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    // alert(state.fieldValue)
+    // send messages to chat server
+    setState(draft => {
+      // Add messages to state collection of messages
+      draft.chatMessages.push({ message: draft.fieldValue, username: appState.user.username, avatar: appState.user.avatar })
+      draft.fieldValue = ''
+    })
+
+  }
+
   return (
     <div id="chat-wrapper" className={"chat-wrapper  shadow border-top border-left border-right " + (appState.isChatOpen ? "chat-wrapper--is-visible" : "")}>
       <div className="chat-title-bar bg-success">
@@ -22,29 +52,36 @@ function Chat() {
         </span>
       </div>
       <div id="chat" className="chat-log">
-        <div className="chat-self">
-          <div className="chat-message">
-            <div className="chat-message-inner">Hey, how are you?</div>
-          </div>
-          <img className="chat-avatar avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" />
-        </div>
-
-        <div className="chat-other">
-          <a href="#">
-            <img className="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128" />
-          </a>
-          <div className="chat-message">
-            <div className="chat-message-inner">
-              <a href="#">
-                <strong>barksalot:</strong>
-              </a>
+        {state.chatMessages.map((message, index) => {
+          if (message.username == appState.user.username) {
+            return (<div className="chat-self">
+              <div className="chat-message">
+                <div className="chat-message-inner">{message.message}</div>
+              </div>
+              <img className="chat-avatar avatar-tiny" src={message.avatar} />
+            </div>)
+          }
+          return (<div className="chat-other">
+            <a href="#">
+              <img className="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128" />
+            </a>
+            <div className="chat-message">
+              <div className="chat-message-inner">
+                <a href="#">
+                  <strong>barksalot:</strong>
+                </a>
               Hey, I am good, how about you?
             </div>
-          </div>
-        </div>
+            </div>
+          </div>)
+        })}
       </div>
-      <form id="chatForm" className="chat-form border-top">
-        <input ref={chatField} type="text" className="chat-field" id="chatField" placeholder="Type a message…" autoComplete="off" />
+      <form onSubmit={handleSubmit} id="chatForm" className="chat-form border-top">
+        <input
+          value={state.fieldValue}
+
+          onChange={handleFieldChange}
+          ref={chatField} type="text" className="chat-field" id="chatField" placeholder="Type a message…" autoComplete="off" />
       </form>
     </div>
   )
